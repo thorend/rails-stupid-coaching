193e948d83f24ef0f36b57e69a31209f141244ec
Thoren Depner <thorendepner@gmail.com>
2023-05-29 14:37:05 +0200

    challenge 2 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'ask', to: 'questions#ask'
   # Defines the root path route ("/")
+  get 'answer', to: 'questions#answer'
   # root "articles#index"
 end
